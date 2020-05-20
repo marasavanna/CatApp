02d7aacfcad938bc54b7a0ca12979abbeedb9d01
Mara Savan <maraasavan@gmail.com>
2020-05-21 00:58:09 +0300

    Make details screen scrollable

--- app/src/main/java/com/example/catapp/bases/MainActivity.kt
+++ app/src/main/java/com/example/catapp/bases/MainActivity.kt
@@ -10,11 +10,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
         setContentView(R.layout.activity_main)
     }
 }
